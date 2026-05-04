7c7827a217b09aa2908eb22bb228e26d19a56153
bekzat066 <kurmangaliev.2001.01@gmail.com>
2026-05-04 12:01:03 +0500

    Update cart.js

--- cart.js
+++ cart.js
@@ -3,14 +3,54 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 const container = document.getElementById("cart-items");
 const totalBlock = document.getElementById("total");
 
-let total = 0;
+function renderCart() {
+    container.innerHTML = "";
+    let total = 0;
 
-cart.forEach(item => {
-    const div = document.createElement("div");
-    div.innerText = item.name + " - " + item.price + " ₸";
-    container.appendChild(div);
+    cart.forEach((item, index) => {
+        const div = document.createElement("div");
+        div.className = "item";
 
-    total += item.price;
-});
+        div.innerHTML = `
+            <img src="${item.img}">
+            <div>
+                <h3>${item.name}</h3>
+                <p>${item.price} ₸</p>
+                <div class="controls">
+                    <button onclick="changeQty(${index}, -1)">➖</button>
+                    ${item.quantity}
+                    <button onclick="changeQty(${index}, 1)">➕</button>
+                    <button onclick="removeItem(${index})">❌</button>
+                </div>
+            </div>
+        `;
 
-totalBlock.innerText = "Итого: " + total + " ₸";
+        container.appendChild(div);
+
+        total += item.price * item.quantity;
+    });
+
+    totalBlock.innerText = "Итого: " + total + " ₸";
+}
+
+function changeQty(index, delta) {
+    cart[index].quantity += delta;
+
+    if (cart[index].quantity <= 0) {
+        cart.splice(index, 1);
+    }
+
+    saveCart();
+}
+
+function removeItem(index) {
+    cart.splice(index, 1);
+    saveCart();
+}
+
+function saveCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+    renderCart();
+}
+
+renderCart();
