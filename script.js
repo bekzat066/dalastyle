533d4c7305507dd697541f0877c1a08406343aa1
bekzat066 <kurmangaliev.2001.01@gmail.com>
2026-05-04 12:00:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function addToCart(name, price) {
-    cart.push({ name, price });
+function addToCart(name, price, img) {
+    const existing = cart.find(item => item.name === name);
+
+    if (existing) {
+        existing.quantity++;
+    } else {
+        cart.push({ name, price, img, quantity: 1 });
+    }
 
     localStorage.setItem("cart", JSON.stringify(cart));
-
     updateCartCount();
 }
 
 function updateCartCount() {
-    document.getElementById("cart-count").innerText = cart.length;
+    let total = 0;
+    cart.forEach(item => total += item.quantity);
+    document.getElementById("cart-count").innerText = total;
 }
 
 function goToCart() {
